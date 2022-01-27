966532f6ae56a7d38d1af0b0a709086ee6e1e659
mangelm <miguelangelmorenodelgadot@gmail.com>
2022-01-27 05:29:01 +0100

    main

---
+++ 1.Primera Pagina PHP/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+print "Bienvenidos a la primera pagina de PHP <br> " ;  //imprime y hace un salto de linea (br)
+
+print "Hola angel <br>";
+
+print "Hasta la proxima actualizacion";
+?>
+</body>
+</html>
+
