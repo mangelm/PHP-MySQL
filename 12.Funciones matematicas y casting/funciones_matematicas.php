575f0adcdf1cf16a58a0f5b720535104bcbfc36b
M. Angel Moreno <59739569+mangelm@users.noreply.github.com>
2022-01-28 08:59:36 +0100

    4.4

---
+++ 12.Funciones matematicas y casting/funciones_matematicas.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funciones matematicas</title>
+</head>
+<body>
+<?php
+
+    //$num1=rand();
+    //$num1=rand(10,50);
+    //$num1=pow(5,3);
+    //$num1=5.25;
+    //$num1=5.7556454;
+    // $num1="5";
+
+    // $num1+=2;
+
+    // $num1+5.75;
+
+    //echo "El número es: " . $num1;
+    //echo "El número es: " . round($num1);
+    //echo "El número es: " . round($num1,2);
+
+    //Casting Explicito
+    $num1="5";
+
+    $resultado=(int)$num1;
+
+
+?>
+</body>
+</html>
