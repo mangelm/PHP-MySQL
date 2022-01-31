11e369a26c9b10ec9d0bbb664fea01c26611e7cf
M. Angel Moreno <59739569+mangelm@users.noreply.github.com>
2022-01-31 07:56:46 +0100

    5

---
+++ 16.Bucles I While y Do While/ejemplo_while_I.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo While I</title>
+</head>
+<body>
+<?php
+
+    //$var1=1;
+    $var1 = 7;
+
+    /* while ($var1<6){
+
+        echo "Estamos ejecutando el codigo del bucle <br>";
+       
+        $var1++;
+
+    }; */
+
+    //Ejecuta almenos una vez aunque sea falsa
+    do{
+
+        echo "Estamos ejecutando el codigo del bucle <br>";
+       
+        $var1++;
+
+    }while$var1<6);
+
+    echo "Hemos salido del bucle";
+?> 
+</body>
+</html>
