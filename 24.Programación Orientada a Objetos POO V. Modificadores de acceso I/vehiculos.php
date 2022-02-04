19458e6d3de89ce82e44606218d2904d01aa28ec
M. Angel Moreno <59739569+mangelm@users.noreply.github.com>
2022-02-04 12:40:05 +0100

    6.4.2

---
+++ 24.Programación Orientada a Objetos POO V. Modificadores de acceso I/vehiculos.php
@@ -0,0 +1,91 @@
+<?php
+
+    //Clase
+    class Coche{
+
+        //Caracteristicas/Propiedades
+        //Encapsulando para que no se pueda modificar desde fuera de la clase
+        //NO podemos acceder mirar la carpeta 25
+        private $ruedas;
+
+        var $color;
+
+        var $motor;
+
+        //Funcionalidades/Metodos
+
+        function Coche(){
+            //Metodo constructor tiene que tener mismo nombre que clase en el php antiguo
+            //Estado inicial
+         
+            $this->ruedas=4;
+
+            $this->color="";
+
+            $this->motor=1600;
+            
+        }
+
+        function arrancar(){
+
+            echo "Estoy arrancado <br>";
+        }
+
+        function girar(){
+
+            echo "Estoy girando <br>";
+        }
+
+        function frenar(){  
+
+            echo "Estoy frenando <br>";
+
+        }
+        
+        function establece_color($color_coche,$nombre_coche){
+        
+        	$this->color=$color_coche;
+            
+            //echo "El color de este coche es:" . $this->color . "<br>";
+            
+            echo "El color de: " . $nombre_coche . " es: " . $this->color . "<br>";
+        }
+    }
+
+    //Clase
+    class Camion extends Coche{
+
+
+        function Camion(){
+            //Metodo constructor tiene que tener mismo nombre que clase en el php antiguo
+            //Estado inicial
+            $this->ruedas=8;
+
+            $this->color="gris";
+
+            $this->motor=1600;
+            
+        }
+
+
+        //Sobreescribir la herencia        
+        function establece_color($color_camion,$nombre_camion){
+        
+        	$this->color=$color_camion;
+            
+            //echo "El color de este coche es:" . $this->color . "<br>";
+            
+            echo "El color de: " . $nombre_camion . " es: " . $this->color . "<br>";
+        }
+
+        //Llamando al metodo de la clase padre (coche) para modificarlo
+        function arrancar(){
+
+            parent::arrancar();
+
+            echo "Camión arrancado";
+        }
+         
+    }
+
+?>     
